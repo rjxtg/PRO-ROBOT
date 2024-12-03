b03fecbd6a8bf79423e365e153fb4e6d5f693506
RJ <147345876+rjxtg@users.noreply.github.com>
2024-12-03 10:10:52 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -41,7 +41,7 @@ async def save_group(bot, message):
                     InlineKeyboardButton('Gʀᴏᴜᴘ', url=GRP_LNK),
                     InlineKeyboardButton('Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                  ],[
-                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/ rjmallu")
+                    InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/rjmallu")
                   ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
